706ec9b3c56947a247f07622fd17d1d43467eb5a
Liu Biyu <lby19981017@gmail.com>
2025-09-29 14:08:18 +0800

    Add Chakra UI system and color mode support
    Integrates Chakra UI and next-themes for color mode management, adds UI provider and color mode components, and refactors Sidebar to use Chakra Button. Also adds react-icons and updates Home page for cleaner markup.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from "./components/ui/provider"
+
 import App from './App.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
+    <Provider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </StrictMode>
 )
